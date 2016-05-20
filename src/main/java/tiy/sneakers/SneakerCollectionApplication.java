fbced198ae938c2b5dedfec15269727c51439f82
Corey Shaw <coreyrshaw@gmail.com>
2016-05-20 15:00:51 -0400

    Baseline for Sneaker Collection.

---
+++ src/main/java/tiy/sneakers/SneakerCollectionApplication.java
@@ -0,0 +1,12 @@
+package tiy.sneakers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SneakerCollectionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SneakerCollectionApplication.class, args);
+	}
+}
---
+++ src/test/java/tiy/sneakers/SneakerCollectionApplicationTests.java
@@ -0,0 +1,18 @@
+package tiy.sneakers;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = SneakerCollectionApplication.class)
+@WebAppConfiguration
+public class SneakerCollectionApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
